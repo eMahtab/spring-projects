9e8c15b60828b1eec63a38079fd593203d4890a6
Mahtab Alam <alammahtab08@gmail.com>
2024-12-27 22:34:52 +0530

    added @Autowired

--- spring-core/stereotype-annotations/src/main/java/net/mahtabalam/service/UserService.java
+++ spring-core/stereotype-annotations/src/main/java/net/mahtabalam/service/UserService.java
@@ -2,10 +2,12 @@ package net.mahtabalam.service;
 
 import net.mahtabalam.model.User;
 import net.mahtabalam.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
