8391801749e56b550ad03a6d8a6fef7f59bc52b5
Mahtab Alam <alammahtab08@gmail.com>
2024-12-28 09:57:09 +0530

    added a simple spring mvc project

---
+++ spring-mvc/spring-mvc-start/src/main/java/net/mahtabalam/springmvc/SpringMvcStartApplication.java
@@ -0,0 +1,13 @@
+package net.mahtabalam.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcStartApplication.class, args);
+	}
+
+}
---
+++ spring-mvc/spring-mvc-start/src/test/java/net/mahtabalam/springmvc/SpringMvcStartApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mahtabalam.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
