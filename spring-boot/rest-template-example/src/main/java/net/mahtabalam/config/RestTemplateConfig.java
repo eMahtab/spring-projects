6aee10059d4f3834bbc2f9279117dc7b8227f08c
Mahtab Alam <alammahtab08@gmail.com>
2025-01-01 12:57:50 +0530

    added timeout for RestTemplate

--- spring-boot/rest-template-example/src/main/java/net/mahtabalam/config/RestTemplateConfig.java
+++ spring-boot/rest-template-example/src/main/java/net/mahtabalam/config/RestTemplateConfig.java
@@ -2,6 +2,7 @@ package net.mahtabalam.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,12 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        int timeout = 4000; // 4 seconds in milliseconds
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        // sets the connection timeout (time to establish the connection)
+        factory.setConnectTimeout(timeout);
+        // sets the read timeout (time to wait for data after connection)
+        factory.setReadTimeout(timeout);
+        return new RestTemplate(factory);
     }
 }
